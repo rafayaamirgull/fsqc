Fixed cc_size and holes computation for recent FastSurfer versions

fsqc/checkCCSize.py
@@ -53,9 +53,9 @@ def checkCCSize(subjects_dir, subject):
         "CC_Anterior",
     ]
 
-    relative_cc = np.nan
-
     sum_cc = 0.0
+
+    intracranial_volume = None
 
     # Loop through the cc elements
     for cc_segmentation in cc_elements:
@@ -64,10 +64,13 @@ def checkCCSize(subjects_dir, subject):
             # If the segmentation is found, compute the sum and return it.
             if cc_segmentation in aseg_stat_line:
                 sum_cc += float(aseg_stat_line.split()[3])
-            elif "EstimatedTotalIntraCranialVol" in aseg_stat_line:
+            elif "# Measure EstimatedTotalIntraCranialVol" in aseg_stat_line:
                 intracranial_volume = float(aseg_stat_line.split(",")[3])
 
-    relative_cc = sum_cc / intracranial_volume
+    if sum_cc > 0.0 and intracranial_volume is not None:
+        relative_cc = sum_cc / intracranial_volume
+    else:
+        relative_cc = np.nan
 
     logging.info(
         "Relative size of the corpus callosum is " + f"{relative_cc:.4}"

fsqc/checkTopology.py
@@ -48,6 +48,8 @@ def checkTopology(subjects_dir, subject):
 
     import numpy as np
 
+    from lapy import TriaMesh
+
     # Settings
 
     logging.captureWarnings(True)
@@ -77,21 +79,30 @@ def checkTopology(subjects_dir, subject):
     topo_time_lh = np.nan
     topo_time_rh = np.nan
 
+    # Extract info form surfaces
+
+    lh_orig_nofix = os.path.join(subjects_dir, subject, "surf", "lh.orig.nofix")
+    if os.path.exists(lh_orig_nofix):
+        lh_euler = TriaMesh.read_fssurf(lh_orig_nofix).euler()
+        lh_holes = int(( 2 - lh_euler ) / 2)
+        logging.info("Number of holes in the left hemisphere: " + str(lh_holes))
+    else:
+        logging.info("Could not find: " + lh_orig_nofix)
+
+    rh_orig_nofix = os.path.join(subjects_dir, subject, "surf", "rh.orig.nofix")
+    if os.path.exists(rh_orig_nofix):
+        rh_euler = TriaMesh.read_fssurf(rh_orig_nofix).euler()
+        rh_holes = int(( 2 - rh_euler ) / 2)
+        logging.info("Number of holes in the right hemisphere: " + str(rh_holes))
+    else:
+        logging.info("Could not find: " + rh_orig_nofix)
+
     # Extract info from logfile
 
     foundDefectsLH = False
     foundTopoLH = False
 
     for line_log_file in lines_log_file:
-        # Look for the number of holes in the left and right hemisphere
-        if "orig.nofix lhholes" in line_log_file:
-            lh_holes = line_log_file.split()[3]
-            lh_holes = lh_holes[:-1]
-            lh_holes = int(lh_holes)
-            rh_holes = line_log_file.split()[6]
-            logging.info("Number of holes in the left hemisphere: " + str(lh_holes))
-            logging.info("Number of holes in the right hemisphere: " + str(rh_holes))
-
         # Look for the number of defects
         if "defects found" in line_log_file and foundDefectsLH is False:
             lh_defects = line_log_file.split()[0]